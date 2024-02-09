5b1d9d123d58a76cd3555bb2cf70d3221e7369de
testcodeuser <test@yahoo.com>
2024-02-09 23:21:07 +0530

    startup script updated

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull sivahariu/simple-python-flask-app
+docker pull sivahariu/pythonproject
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 sivahariu/simple-python-flask-app
+docker run -d -p 5000:5000 sivahariu/pythonproject
